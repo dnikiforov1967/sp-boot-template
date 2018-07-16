dcee260da753df4103fb7a69b8c05e8d36cf1d82
Dmitry Nikiforov <dmitry.nikiforov@adition.com>
2018-07-16 11:37:31 +0200

    Minimal web application multi-module project

---
+++ sp-boot-template-head/src/main/java/org/template/spboot/head/MainApplication.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.template.spboot.head;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.template.spboot.web.WebApplicationConfig;
+
+/**
+ *
+ * @author dnikiforov
+ */
+public class MainApplication {
+	
+	public static void main(String... args) {
+		final ConfigurableApplicationContext run = new SpringApplicationBuilder()
+				.parent(WebApplicationConfig.class)
+				.web(WebApplicationType.SERVLET)
+				.run(args);
+	}
+	
+}
---
+++ sp-boot-template-web/src/main/java/org/template/spboot/web/WebApplicationConfig.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.template.spboot.web;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author dnikiforov
+ */
+@SpringBootApplication
+public class WebApplicationConfig {
+	
+}
