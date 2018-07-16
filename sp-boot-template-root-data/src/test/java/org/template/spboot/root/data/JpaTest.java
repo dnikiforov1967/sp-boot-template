2c3b7c8bbc5cfb96926a8385ac810d11115f22d3
Dmitry Nikiforov <dmitry.nikiforov@adition.com>
2018-07-16 15:23:50 +0200

    Create spring boot test for particular configuration

---
+++ sp-boot-template-root-data/src/test/java/org/template/spboot/root/data/JpaTest.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.template.spboot.root.data;
+
+import java.util.Set;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.template.spboot.root.data.jpa.RoleJpaInterface;
+import org.template.spboot.root.data.jpa.UserAppJpaInterface;
+import org.template.spboot.root.data.model.AppRole;
+import org.template.spboot.root.data.model.AppUser;
+import static org.template.spboot.root.data.model.RoleEnum.ROLE_ADMIN;
+import static org.template.spboot.root.data.model.RoleEnum.ROLE_USER;
+
+/**
+ *
+ * @author dnikiforov
+ */
+@RunWith(SpringRunner.class)
+@EnableAutoConfiguration
+@ContextConfiguration(classes = {DatabaseConfiguration.class})
+@Transactional
+public class JpaTest {
+	
+	@Autowired
+	private UserAppJpaInterface apiUser;
+	@Autowired
+	private RoleJpaInterface apiRole;
+	@PersistenceContext(unitName = "globalPU")
+	private EntityManager em;
+	
+	public JpaTest() {
+	}
+	
+	@BeforeClass
+	public static void setUpClass() {
+	}
+	
+	@AfterClass
+	public static void tearDownClass() {
+	}
+	
+	@Before
+	public void setUp() {
+	}
+	
+	@After
+	public void tearDown() {
+	}
+
+	// TODO add test methods here.
+	// The methods must be annotated with annotation @Test. For example:
+	//
+	@Test
+	public void testCreateUser() {
+		AppUser appUser = new AppUser("probe","probe");
+		appUser.getAppRoles().add(new AppRole(ROLE_USER));
+		appUser.getAppRoles().add(new AppRole(ROLE_ADMIN));
+		final AppUser saved = apiUser.saveAndFlush(appUser);
+		final Set<AppRole> appRoles = saved.getAppRoles();
+		assertFalse(appRoles.isEmpty());
+		assertEquals(2, appRoles.size());
+		em.clear();
+		final AppUser find = em.find(AppUser.class, "probe");
+		final Set<AppRole> findAppRoles = find.getAppRoles();
+		assertFalse(findAppRoles.isEmpty());
+		assertEquals(2, findAppRoles.size());
+	}
+}
