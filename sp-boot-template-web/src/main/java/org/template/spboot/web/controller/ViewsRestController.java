d255c25ae6ad3e6704d18bdc692f52e177cd14b5
dnikiforov1967 <dnikiforov1967@gmail.com>
2018-07-26 23:27:33 +0200

    Append controller based on model &view

---
+++ sp-boot-template-web/src/main/java/org/template/spboot/web/controller/ViewsRestController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.template.spboot.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author dima
+ */
+@RestController
+@RequestMapping("/views")
+public class ViewsRestController {
+
+    @RequestMapping("/modelview")
+    public ModelAndView getModelView() {
+        System.out.println("MODEL AND VIEW");
+        final ModelAndView modelAndView = new ModelAndView("simpleView");
+        modelAndView.addObject("name", "Dima");
+        return modelAndView;
+    }
+
+}
