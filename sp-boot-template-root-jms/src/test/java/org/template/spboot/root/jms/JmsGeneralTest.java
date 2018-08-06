8e5d081dbcfb8cd773766ac524de1811b4fc1563
Dmitry Nikiforov <dmitry.nikiforov@adition.com>
2018-08-06 18:37:55 +0200

    Imports

--- sp-boot-template-root-jms/src/test/java/org/template/spboot/root/jms/JmsGeneralTest.java
+++ sp-boot-template-root-jms/src/test/java/org/template/spboot/root/jms/JmsGeneralTest.java
@@ -23,7 +23,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
