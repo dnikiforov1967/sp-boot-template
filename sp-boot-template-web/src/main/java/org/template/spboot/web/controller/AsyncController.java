f4d0b081e8c3e26496ef04ed23bdbb46d6672f7b
Dmitry Nikiforov <dmitry.nikiforov@adition.com>
2018-08-14 15:51:19 +0200

    Append method with deferred result

--- sp-boot-template-web/src/main/java/org/template/spboot/web/controller/AsyncController.java
+++ sp-boot-template-web/src/main/java/org/template/spboot/web/controller/AsyncController.java
@@ -5,11 +5,14 @@
  */
 package org.template.spboot.web.controller;
 
+import com.fasterxml.jackson.databind.ser.std.StdKeySerializers;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 import org.template.spboot.root.async.TaskInterface;
 
 
@@ -28,6 +31,14 @@ public class AsyncController {
 	public Callable<String> getSimpleAsync() {
 		Callable<String> callable = taskService::execute;
 		return callable;
+	}
+
+	@RequestMapping(path = "/def", method = GET)
+	public DeferredResult<String> getSimpleAsyncDef() {
+		DeferredResult<String> dr = new DeferredResult<>();
+		final CompletableFuture<String> supplyAsync = CompletableFuture.supplyAsync(taskService::execute);
+		supplyAsync.whenCompleteAsync((result, throwable) -> dr.setResult(result));
+		return dr;
 	}
 	
 }
