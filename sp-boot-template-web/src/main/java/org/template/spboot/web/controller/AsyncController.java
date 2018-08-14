99dada4c2ef730df5992fc916b6a412f04eb0523
Dmitry Nikiforov <dmitry.nikiforov@adition.com>
2018-08-14 17:21:32 +0200

    Append exception-throw method

--- sp-boot-template-web/src/main/java/org/template/spboot/web/controller/AsyncController.java
+++ sp-boot-template-web/src/main/java/org/template/spboot/web/controller/AsyncController.java
@@ -5,16 +5,18 @@
  */
 package org.template.spboot.web.controller;
 
-import com.fasterxml.jackson.databind.ser.std.StdKeySerializers;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 import org.template.spboot.root.async.TaskInterface;
-
 
 /**
  *
@@ -25,8 +27,13 @@ import org.template.spboot.root.async.TaskInterface;
 public class AsyncController {
 
 	@Autowired
+	@Qualifier("taskService")
 	private TaskInterface taskService;
-	
+
+	@Autowired
+	@Qualifier("taskServiceEx")
+	private TaskInterface taskServiceEx;
+
 	@RequestMapping(path = "", method = GET)
 	public Callable<String> getSimpleAsync() {
 		Callable<String> callable = taskService::execute;
@@ -40,5 +47,23 @@ public class AsyncController {
 		supplyAsync.whenCompleteAsync((result, throwable) -> dr.setResult(result));
 		return dr;
 	}
-	
+
+	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "I have catched it")
+	@ExceptionHandler(ArithmeticException.class)
+	@RequestMapping(path = "/defex", method = GET)
+	public DeferredResult<String> getSimpleAsyncDefEx() {
+		DeferredResult<String> dr = new DeferredResult<>();
+		final CompletableFuture<String> supplyAsync = CompletableFuture.supplyAsync(taskServiceEx::execute);
+		supplyAsync.whenCompleteAsync(
+				(result, throwable) -> {
+					if (throwable != null) {
+						dr.setErrorResult(throwable);
+					} else {
+						dr.setResult("OK");
+					}
+				}
+		);
+		return dr;
+	}
+
 }
