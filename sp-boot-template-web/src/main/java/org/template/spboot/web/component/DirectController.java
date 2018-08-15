df8d68b4fa74b23a89fce7a1795e4c59ac6e8c04
Dmitry Nikiforov <dmitry.nikiforov@adition.com>
2018-08-15 10:37:14 +0200

    Implement class what implement MVC Controller interface

---
+++ sp-boot-template-web/src/main/java/org/template/spboot/web/component/DirectController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.template.spboot.web.component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+/**
+ *
+ * @author dnikiforov
+ */
+public class DirectController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("direct");
+		mav.addObject("greeting", "Hello from direct mvc controller");
+		return mav;
+	}
+	
+}
