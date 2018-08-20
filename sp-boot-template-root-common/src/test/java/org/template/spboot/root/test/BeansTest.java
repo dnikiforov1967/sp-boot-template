73f4538244c137b7dfe64f9a593e4eb39f4fea68
dnikiforov1967 <dnikiforov1967@gmail.com>
2018-08-20 21:35:15 +0200

    Unused import

--- sp-boot-template-root-common/src/test/java/org/template/spboot/root/test/BeansTest.java
+++ sp-boot-template-root-common/src/test/java/org/template/spboot/root/test/BeansTest.java
@@ -16,7 +16,6 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 import org.template.spboot.root.beans.SimpleBean;
 
 /**
